d0d9d5ffaff2b627324b8e16bf1321f039f263d9
Mienke Dreyer <mienke.hattingh@gmail.com>
2023-05-30 20:23:55 +1000

    Session 1 practice

---
+++ session_1/variables.py
@@ -0,0 +1,55 @@
+# #create a variable named first_name and assign Mienke - use hash to take notes
+# first_name = "Mienke"
+# last_name = "Dreyer"
+# programming_language = "python"
+
+# # Data types - text data, numerical data
+# #text data - string - always need to put quotes around info. could be single or double quptation signs
+# #convention - all lowercase with underscore between
+
+# today = "Tuesday"
+# #print is command to see on screen
+# # print(today)
+# # #f" - lets you use variable sinside.
+# # print(f"today is {today}")
+# # print(type(today))
+
+# #print as you gp to keep track wgat you are busy with
+
+
+# #numerical data types - integer, float
+# #Integer - whole number - dont need to use quotes
+# parkers_age = 4
+# print(type(parkers_age))
+# print(f"Parker is turning {parkers_age+1} soon!")
+# height = 155
+# print(f"my height is {height/100} in m.")
+# #float
+# weight = 57.8
+# print(f"my weight is{weight*2.1} in pounds")
+
+
+# distance = 5000
+# #change the distance into an integer
+# print(int(distance)+9)
+# #expecting more input from user if using input
+
+# #input function stores the data as string
+# # dog_name = input("What is your dog's name?")
+# # print(f"nice to meet you {dog_name}")
+# birth_year = input("What year were you born?")
+# print(f"you are {2023-int(birth_year)}")
+
+
+# # #excercise 1
+# first_number = input("Enter a number")
+# second_number = input("enter another number")
+# #print(int(first_number)+int(second_number))
+
+
+# #excercise 2
+# print(int(first_number)*int(second_number))
+
+km = input("How many kilometers")
+print(f"That would be {int(km)*1000}m")
+print(f"or {int(km)*100000}cm")
